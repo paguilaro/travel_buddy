4ce55ca8a04a5bf3d288fbcbb8dd9c4b2985e0f7
paguilaro <paula.aguilar@gmail.com>
2021-09-10 21:16:59 -0400

    templates y rutas basicas

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views, auth
+urlpatterns = [
+    path('', views.index),
+    path('addtrip', views.addtrip),
+    path('registro', auth.registro),
+    path('login', auth.login),
+    path('logout', auth.logout)
+]
---
+++ main/views.py
@@ -0,0 +1,21 @@
+from django.contrib import messages
+from django.shortcuts import redirect, render
+import bcrypt
+from .decorators import login_required
+
+
+@login_required
+def index(request):
+
+    context = {
+        'saludo': 'Hola'
+    }
+    return render(request, 'index.html', context)
+
+@login_required
+def addtrip(request):
+
+    context = {
+        'saludo': 'Hola'
+    }
+    return render(request, 'addtrip.html', context)
